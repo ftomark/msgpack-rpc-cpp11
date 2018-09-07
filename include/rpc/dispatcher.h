5518be903210dc973ffd14cc3e9030431e0cb836
mark <mark@markMacBook.local>
2018-09-08 01:31:13 +0800

    create project

---
+++ include/rpc/dispatcher.h
@@ -0,0 +1,142 @@
+#pragma once
+
+#ifndef DISPATCHER_H_CXIVZD5L
+#define DISPATCHER_H_CXIVZD5L
+
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <unordered_map>
+
+#include "rpc/config.h"
+#include "rpc/msgpack.hpp"
+
+#include "rpc/detail/call.h"
+#include "rpc/detail/func_tools.h"
+#include "rpc/detail/func_traits.h"
+#include "rpc/detail/log.h"
+#include "rpc/detail/not.h"
+#include "rpc/detail/response.h"
+#include "rpc/detail/make_unique.h"
+
+namespace rpc {
+
+namespace detail {
+	class EventManager
+	{//负责保存函数对象，负责绑定，与dispatcher解耦
+		public:
+			EventManager(){};
+
+			virtual EventManager& operator=(EventManager& a)
+			{
+				funcs_=a.funcs_;
+				return *this;
+			}
+
+			//! \brief This functor type unifies the interfaces of functions that are
+			//!        called remotely
+			using adaptor_type = std::function<std::unique_ptr<RPCLIB_MSGPACK::object_handle>(
+					RPCLIB_MSGPACK::object const &)>;
+
+			std::unordered_map<std::string, adaptor_type> funcs_;
+
+			//! \brief Checks the argument count and throws an exception if
+			//! it is not the expected amount.
+			static void enforce_arg_count(std::string const &func, std::size_t found,
+					std::size_t expected);
+
+			void enforce_unique_name(std::string const &func);
+			//! \brief Binds a functor to a name so it becomes callable via RPC.
+			//! \param name The name of the functor.
+			//! \param func The functor to bind.
+			//! \tparam F The type of the functor.
+			template <typename F> void bind(std::string const &name, F func);
+
+			//! \defgroup Tag-dispatched bind implementations for various functor cases.
+
+			//! \brief Stores a void, zero-arg functor with a name.
+			template <typename F>
+				void bind(std::string const &name, F func,
+						detail::tags::void_result const &,
+						detail::tags::zero_arg const &);
+
+			//! \brief Stores a void, non-zero-arg functor with a name.
+			template <typename F>
+				void bind(std::string const &name, F func,
+						detail::tags::void_result const &,
+						detail::tags::nonzero_arg const &);
+
+			//! \brief Stores a non-void, zero-arg functor with a name.
+			template <typename F>
+				void bind(std::string const &name, F func,
+						detail::tags::nonvoid_result const &,
+						detail::tags::zero_arg const &);
+
+			//! \brief Stores a non-void, non-zero-arg functor with a name.
+			template <typename F>
+				void bind(std::string const &name, F func,
+						detail::tags::nonvoid_result const &,
+						detail::tags::nonzero_arg const &);
+
+			template <typename T> RPCLIB_MSGPACK::object pack(T &&arg);
+	};
+
+
+//! \brief This class maintains a registry of functors associated with their
+//! names, and callable using a msgpack-rpc call pack.
+//dispatcher 抽象类，不包含bind,通过EventManager进行函数绑定管理
+class default_dispatcher
+{
+public:
+    
+    EventManager event_handle;
+    //! \brief This is the type of messages as per the msgpack-rpc spec.
+    using call_t = std::tuple<int8_t, uint32_t, std::string, RPCLIB_MSGPACK::object>;
+
+    //! \brief This is the type of notification messages.
+    using notification_t = std::tuple<int8_t, std::string, RPCLIB_MSGPACK::object>;
+
+
+    //! \brief Processes a message that contains a call according to
+    //! the Msgpack-RPC spec.
+    //! \param msg The buffer that contains the messagepack.
+    //! \throws std::runtime_error If the messagepack does not contain a
+    //! a call or the types of the parameters are not convertible to the called
+    //! functions' parameters.
+    virtual void dispatch(RPCLIB_MSGPACK::sbuffer const &msg);
+
+    //! \brief Processes a message that contains a call according to
+    //! the Msgpack-RPC spec.
+    //! \param msg The messagepack object that contains the call.
+    //! \param suppress_exceptions If true, exceptions will be caught and
+    //! written
+    //! as response for the client.
+    //! \throws std::runtime_error If the types of the parameters are not
+    //! convertible to the called functions' parameters.
+    virtual detail::response dispatch(RPCLIB_MSGPACK::object const &msg,
+                              bool suppress_exceptions = false);
+
+    enum class request_type { notification=0, call=1 };
+    
+    //template <typename T> RPCLIB_MSGPACK::object pack(T &&arg);
+
+private:
+
+	//! \brief Dispatches a call (which will have a response).
+    virtual detail::response dispatch_call(RPCLIB_MSGPACK::object const &msg,
+                                   bool suppress_exceptions = false);
+	
+
+    //! \brief Dispatches a notification (which will not have a response)
+    virtual detail::response dispatch_notification(RPCLIB_MSGPACK::object const &msg,
+                                           bool suppress_exceptions = false);
+
+
+    RPCLIB_CREATE_LOG_CHANNEL(default_dispatcher)
+};
+}//detail
+}//rpc
+
+#include "dispatcher.inl"
+
+#endif /* end of include guard: DISPATCHER_H_CXIVZD5L */
---
+++ lib/rpc/dispatcher.cc
@@ -0,0 +1,155 @@
+#include "rpc/dispatcher.h"
+#include "format.h"
+#include "rpc/detail/client_error.h"
+#include "rpc/this_handler.h"
+
+namespace rpc {
+namespace detail {
+
+using detail::response;
+
+
+
+void default_dispatcher::dispatch(RPCLIB_MSGPACK::sbuffer const &msg) {
+    auto unpacked = RPCLIB_MSGPACK::unpack(msg.data(), msg.size());
+    dispatch(unpacked.get());
+}
+
+response default_dispatcher::dispatch(RPCLIB_MSGPACK::object const &msg,
+                              bool suppress_exceptions) {
+    call_t the_call;
+    msg.convert(the_call);
+	auto &&type = std::get<0>(the_call);
+
+
+	//应该根据msg第一个字段来判断转发,第一个字段代表调用类型
+    switch (type) {
+    case 0:
+        return dispatch_notification(msg, suppress_exceptions);
+    case 2:
+        return dispatch_call(msg, suppress_exceptions);
+    default:
+        return response::empty();
+    }
+}
+
+response default_dispatcher::dispatch_call(RPCLIB_MSGPACK::object const &msg,
+                                   bool suppress_exceptions) {
+    call_t the_call;
+    msg.convert(the_call);
+
+    // TODO: proper validation of protocol (and responding to it)
+    // auto &&type = std::get<0>(the_call);
+    // assert(type == 0);
+
+    auto &&id = std::get<1>(the_call);
+    auto &&name = std::get<2>(the_call);
+    auto &&args = std::get<3>(the_call);
+
+    auto it_func = event_handle.funcs_.find(name);
+
+    if (it_func != end(event_handle.funcs_)) {
+        LOG_DEBUG("Dispatching call to '{}'", name);
+        try {
+            auto result = (it_func->second)(args);
+            return response::make_result(id, std::move(result));
+        } catch (rpc::detail::client_error &e) {
+            return response::make_error(
+                id, RPCLIB_FMT::format("rpclib: {}", e.what()));
+        } catch (std::exception &e) {
+            if (!suppress_exceptions) {
+                throw;
+            }
+            return response::make_error(
+                id,
+                RPCLIB_FMT::format("rpclib: function '{0}' (called with {1} "
+                                   "arg(s)) "
+                                   "threw an exception. The exception "
+                                   "contained this information: {2}.",
+                                   name, args.via.array.size, e.what()));
+        } catch (rpc::detail::handler_error &) {
+            // doing nothing, the exception was only thrown to
+            // return immediately
+        } catch (rpc::detail::handler_spec_response &) {
+            // doing nothing, the exception was only thrown to
+            // return immediately
+        } catch (...) {
+            if (!suppress_exceptions) {
+                throw;
+            }
+            return response::make_error(
+                id,
+                RPCLIB_FMT::format("rpclib: function '{0}' (called with {1} "
+                                   "arg(s)) threw an exception. The exception "
+                                   "is not derived from std::exception. No "
+                                   "further information available.",
+                                   name, args.via.array.size));
+        }
+    }
+    return response::make_error(
+        id, RPCLIB_FMT::format("rpclib: server could not find "
+                               "function '{0}' with argument count {1}.",
+                               name, args.via.array.size));
+}
+
+
+
+response default_dispatcher::dispatch_notification(RPCLIB_MSGPACK::object const &msg,
+                                           bool suppress_exceptions) {
+    notification_t the_call;
+    msg.convert(the_call);
+
+    // TODO: proper validation of protocol (and responding to it)
+    // auto &&type = std::get<0>(the_call);
+    // assert(type == static_cast<uint8_t>(request_type::notification));
+
+    auto &&name = std::get<1>(the_call);
+    auto &&args = std::get<2>(the_call);
+
+    auto it_func = event_handle.funcs_.find(name);
+
+    if (it_func != end(event_handle.funcs_)) {
+        LOG_DEBUG("Dispatching call to '{}'", name);
+        try {
+            auto result = (it_func->second)(args);
+        } catch (rpc::detail::handler_error &) {
+            // doing nothing, the exception was only thrown to
+            // return immediately
+        } catch (rpc::detail::handler_spec_response &) {
+            // doing nothing, the exception was only thrown to
+            // return immediately
+        } catch (...) {
+            if (!suppress_exceptions) {
+                throw;
+            }
+        }
+    }
+    return response::empty();
+}
+
+
+void EventManager::enforce_arg_count(std::string const &func, std::size_t found,
+                                   std::size_t expected) {
+    using detail::client_error;
+    if (found != expected) {
+        throw client_error(
+            client_error::code::wrong_arity,
+            RPCLIB_FMT::format(
+                "Function '{0}' was called with an invalid number of "
+                "arguments. Expected: {1}, got: {2}",
+                func, expected, found));
+    }
+}
+
+
+void EventManager::enforce_unique_name(std::string const &func) {
+    auto pos = funcs_.find(func);
+    if (pos != end(funcs_)) {
+        throw std::logic_error(
+            RPCLIB_FMT::format("Function name already bound: '{}'. "
+                               "Please use unique function names", func));
+    }
+}
+
+}
+} /* rpc */
